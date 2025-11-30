8208826fc066ef94d1be6357a2ccc21fc5c1d078
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-11-30 17:34:46 +0530

    Sort Names

--- src/Week_03/sortNames.cpp
+++ src/Week_03/sortNames.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
 
-class SortNames
-{
+class SortNames{
 public:
     string name;
 
-    void sortList(SortNames arr[], int n)
-    {
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (arr[i].name > arr[j].name)
-                {
-                    swap(arr[i].name, arr[j].name);
-                }
+    void sortList(SortNames arr[], int n);
+    void display(SortNames arr[], int n);
+};
+
+
+
+void SortNames::sortList(SortNames arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            if(arr[i].name > arr[j].name){
+                swap(arr[i].name, arr[j].name); 
             }
         }
     }
+}
 
-    void display(SortNames arr[], int n)
-    {
-        cout << "\nSorted Names:\n";
-        for (int i = 0; i < n; i++)
-            cout << arr[i].name << endl;
-    }
-};
+void SortNames::display(SortNames arr[], int n){
+    cout << "\nSorted Names:\n";
+    for(int i = 0; i < n; i++)
+        cout << arr[i].name << endl;
+}
 
-int main()
-{
+
+
+int main(){
     int n;
-    cout << "Enter number of names: ";
-    cin >> n;
+    cout<<"Enter number of names: ";
+    cin>>n;
+    cin.ignore();  // This is important to clear input buffer for getline()
 
     SortNames arr[n];
 
-    cout << "Enter " << n << " names:\n";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i].name;
+    cout<<"Enter "<<n<<" names:\n";
+    for(int i = 0; i < n; i++)
+        getline(cin, arr[i].name);   
 
     SortNames obj;
     obj.sortList(arr, n);
