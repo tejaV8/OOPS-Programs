a6278a179b475b633b941bb8770126d6a1c13f78
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-11-30 17:29:05 +0530

    Sort Names

---
+++ src/Week_03/sortNames.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class SortNames
+{
+public:
+    string name;
+
+    void sortList(SortNames arr[], int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i].name > arr[j].name)
+                {
+                    swap(arr[i].name, arr[j].name);
+                }
+            }
+        }
+    }
+
+    void display(SortNames arr[], int n)
+    {
+        cout << "\nSorted Names:\n";
+        for (int i = 0; i < n; i++)
+            cout << arr[i].name << endl;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter number of names: ";
+    cin >> n;
+
+    SortNames arr[n];
+
+    cout << "Enter " << n << " names:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i].name;
+
+    SortNames obj;
+    obj.sortList(arr, n);
+    obj.display(arr, n);
+
+    return 0;
+}
